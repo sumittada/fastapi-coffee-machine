9f231263d3f6657dbdf46277207189399de4f14e
Sumit Tada <sumittada@gmail.com>
2022-10-16 14:05:15 +0200

    AWS CDK config to deploy to ECS using Fargate

---
+++ cdk/fastapi_stack.py
@@ -0,0 +1,49 @@
+from aws_cdk import Stack
+from constructs import Construct
+
+import aws_cdk.aws_ec2 as ec2
+import aws_cdk.aws_ecs as ecs
+import aws_cdk.aws_ecs_patterns as ecs_patterns
+
+
+class FastAPIStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        self.vpc = ec2.Vpc(self, "CoffeeMachineVPC", max_azs=3)
+
+        self.ecs_cluster = ecs.Cluster(
+            self,
+            "CoffeeMachineECSCluster",
+            vpc=self.vpc,
+        )
+
+        image = ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+            image=ecs.ContainerImage.from_asset(
+                directory="../src",
+            )
+        )
+
+        self.ecs_service = ecs_patterns.ApplicationLoadBalancedFargateService(
+            self,
+            "FastAPIService",
+            cluster=self.ecs_cluster,
+            cpu=256,
+            memory_limit_mib=512,
+            desired_count=1,
+            task_image_options=image,
+        )
+
+        scalable_target = self.ecs_service.service.auto_scale_task_count(
+            min_capacity=1,
+            max_capacity=20
+        )
+
+        scalable_target.scale_on_cpu_utilization("CpuScaling",
+            target_utilization_percent=50
+        )
+
+        scalable_target.scale_on_memory_utilization("MemoryScaling",
+            target_utilization_percent=50
+        )
+
